45f4bbc1ab1af94ee245ac9b0c2c51bc0cdbbba5
Mickey Poblete <mikhael.poblete@gmail.com>
2020-06-06 17:30:54 -0700

    changed map initation script based on google tutorial. moved from index.html to logic.js. added API key to the logic function using encryption.

--- assets/logic.js
+++ assets/logic.js
@@ -9,3 +9,18 @@ var map;
           zoom: 8
         });
       }
+
+      // Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key='+config.mykey+'&callback=initMap';
+script.defer = true;
+script.async = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function() {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+      
